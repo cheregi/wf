914a1ceb6371e9b367a3d8d003eb3acac983b2d4
cheregi <cheregi.diana@yahoo.com>
2019-02-28 16:21:48 +0100

    "exo_7-solved"

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,30 @@
 <?php
+function divide (int $value, int $divisor):float
+{
+    if ($divisor == 0) {
+        throw new RuntimeException('Deivision by 0 is not allowed');
+    }
+    return (float)($value / $divisor);
+}
+//    try {
+//        divide(10,0);
+//    } catch (RuntimeException $exception) {
+//        echo $exception->getMessage();
+//    }
+//}
+function arrayDivide(array $baseArray, int $divisor) : array {
+    $result =[];
+    foreach ($baseArray as $base) {
+            try {
+                $result[]=divide($base, $divisor);
+            } catch (RuntimeException $exception) {
+                echo "<br>".$exception->getMessage();
+                echo"<br> Using ".$base;
+                $result[]=$base;
+            }
+        }
+    return $result;
+    }
+
+
+print_r( arrayDivide([16,5.6], 0));
