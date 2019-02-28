fd5aca870d5ba3d69a4026e4b74b5c3589dff16f
cheregi <cheregi.diana@yahoo.com>
2019-02-28 14:34:31 +0100

    "exo_6-solved"

--- PHP/Exercice6/exercise.php
+++ PHP/Exercice6/exercise.php
@@ -1,1 +1,29 @@
 <?php
+
+
+function easterReverse(string $FilenameToEdit) {
+//    $filenameOriginal="file.txt";
+//    $filenameToWrite="fileTemp.txt";
+//
+//    copy($filenameOriginal, $filenameToWrite);
+
+
+$fileContent =file_get_contents($FilenameToEdit);
+//echo "$fileContent";
+//echo "<br> Halfway: ".floor(strlen($fileContent)/2);
+
+$secondPart=substr($fileContent, floor(strlen($fileContent)/2));
+$firstPart=substr($fileContent,0, strlen($secondPart)-1);
+
+//echo "<br> firstPart:<br>".$firstPart."\n";
+//echo "<br> secondPart:<br>".$secondPart."\n";
+
+//echo "<br> secondPartReversed:<br>".strrev($secondPart);
+
+$file = fopen($FilenameToEdit,"r+");
+fseek($file, strlen($firstPart), SEEK_SET);
+fwrite($file, strrev($secondPart), strlen($secondPart));
+fclose($file);
+}
+easterReverse("file.txt");
+
