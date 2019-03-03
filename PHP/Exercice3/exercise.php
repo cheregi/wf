bbc4024264dd140658835760c0fba83331d2bf05
cheregi <cheregi.diana@ayhoo.com>
2019-03-03 01:19:59 +0100

    "exo_3 - homework"

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,23 @@
 <?php
-
+$A=0;
+$B=0;
 $input;
-$winner;
+for($i=0; $i<count($input); $i++) {
+    $elemCurrent=$input[$i];
+    
+    //$input[$i][0]
+    //if($input[$i][0]>$input[$i][1])
+    if($elemCurrent[0]>$elemCurrent[1]) {
+        $A++;
+    }else if($elemCurrent[0]<$elemCurrent[1]) {
+        $B++;
+    };
+};
+if($A>$B) {
+    $winner='A';
+};
+if($A<$B) {
+    $winner='B';
+}
+
+
