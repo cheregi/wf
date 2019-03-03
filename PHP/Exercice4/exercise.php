5304ba6672a8f1a99abb76bf6c151c64dce1fc0e
cheregi <cheregi.diana@ayhoo.com>
2019-03-03 16:43:44 +0100

    "exo_4 - homework"

--- PHP/Exercice4/exercise.php
+++ PHP/Exercice4/exercise.php
@@ -1,1 +1,16 @@
 <?php
+function getAllMondaysOfMonth(int $year, int $month) {
+    $result=[];
+    $firstMonday=strtotime('next Monday', mktime(0,0,0,$month,0, $year));
+    array_push($result, date('l j, M Y', $firstMonday));
+    $nextMonday=$firstMonday;
+    $thisMonth=$month;
+    while($thisMonth==$month) {
+        $nextMonday=strtotime('next Monday' . date('Y-m-d',$nextMonday));
+        $thisMonth=date_parse(date('Y-m-d',$nextMonday))['month'];
+        if($thisMonth==$month) {
+            array_push($result, date('l j, M Y', $nextMonday));
+        }
+    }
+    return $result;
+};
