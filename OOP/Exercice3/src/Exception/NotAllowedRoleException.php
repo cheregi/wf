45686cb4a327f4f16be95d8df482b124c85beed3
cheregi <cheregi.diana@yahoo.com>
2019-03-26 15:33:00 +0100

    "has bugs"

---
+++ OOP/Exercice3/src/Exception/NotAllowedRoleException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Exception;
+
+use Throwable;
+
+class NotAllowedRoleException extends \RuntimeException
+{
+
+
+    public function __construct(array $roles, string $label, $message = "", $code = 0, Throwable $previous = null)
+    {
+        $tmpMessage = 'The role ' . $label . ' is not allowed';
+        $tmpMessage .= ' only ' . implode(',', $roles) . ' are allowed ';
+        $tmpMessage .= $message;
+        parent::__construct($message, $code, $previous);
+    }
+
+
+}
+
+//throw new NotAllowedRoleException(
+//    ['user', 'admin'],
+//    'alan',
+//    'Hey!! how are you'
+//);
